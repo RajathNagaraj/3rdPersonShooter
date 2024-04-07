89724a6fc4059db98bc75bcc27f263869fde2697
Rajath Nagaraj <63661481+RajathNagaraj@users.noreply.github.com>
2024-04-07 21:44:57 +0530

    Added animation
    Added animation for soldier with animator controller and respective code

--- 3rdPersonShooter/Assets/Scripts/PlayerMovement.cs
+++ 3rdPersonShooter/Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,27 @@ public class PlayerMovement : MonoBehaviour
     private InputActions inputActions;
     private CharacterController characterController;
     private float moveSpeed;
+    private Animator animator;
 
 
     void Awake()
     {      
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
     // Start is called before the first frame update
     void Start()
     {
         inputActions = GameManager.Instance.inputActions;
-        moveSpeed = 300f;
+        moveSpeed = 1500f;
     }
 
     // Update is called once per frame
     void Update()
     {
         var movementVector2D =  inputActions.OnFoot.Move.ReadValue<Vector2>();
+        float animSpeed = movementVector2D.magnitude;
+        animator.SetFloat("Speed",animSpeed);
         Vector3 movement = new Vector3(movementVector2D.x, 0, movementVector2D.y);
         characterController.SimpleMove(movement * moveSpeed * Time.deltaTime);
 
