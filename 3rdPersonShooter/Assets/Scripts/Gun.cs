9b1f92e9f4c6d5166a075be5b0b141b738b73e27
Rajath Nagaraj <63661481+RajathNagaraj@users.noreply.github.com>
2024-04-24 12:52:51 +0530

    Update Gun.cs
    Commented Debug statement in Gun script

--- 3rdPersonShooter/Assets/Scripts/Gun.cs
+++ 3rdPersonShooter/Assets/Scripts/Gun.cs
@@ -39,7 +39,7 @@ public class Gun : MonoBehaviour
 
     private void FireGun()
     {
-        Debug.DrawRay(firePoint.position,firePoint.forward * 100, Color.red, 2f);
+        //Debug.DrawRay(firePoint.position,firePoint.forward * 100, Color.red, 2f);
         Ray ray = new Ray(firePoint.position,firePoint.forward);
         RaycastHit hitInfo;
         if(Physics.Raycast(ray, out hitInfo, 100f))
