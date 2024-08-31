ebfd73cde695d4df237c7fa3b34fb44e3104e54e
Rajath Nagaraj <63661481+RajathNagaraj@users.noreply.github.com>
2024-08-31 19:22:33 +0530

    Added CameraController script
    Added feature to look up and down on the mouse and have the camera follow that movement.

---
+++ 3rdPersonShooter/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private InputActions inputActions;
+    private CinemachineComposer composer;
+    [SerializeField]
+    private float turnDampener = 0.01f;
+
+    private void Start()
+    {
+        inputActions = GameManager.Instance.inputActions;
+        composer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineComposer>();
+    }
+
+    
+   private void Update()
+    {
+        var lookVector = inputActions.OnFoot.Look.ReadValue<Vector2>();
+        composer.m_TrackedObjectOffset.y += (lookVector.y * turnDampener);
+    }
+}
